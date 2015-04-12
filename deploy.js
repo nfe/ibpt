f5530fde2f8d536f2398b9682b1ae3ee029d4533
Gabriel Marquez <gabriel@nfe.io>
2015-04-12 16:49:50 -0300

    implements travis ci and deploy to azure cdn

---
+++ deploy.js
@@ -0,0 +1,49 @@
+require("dotenv").load(); // load environment variables from .env
+var deploy = require("deploy-azure-cdn");
+var glob = require("glob");
+
+// environment variables
+var azure_account = process.env.AZURE_ACCOUNT,
+    azure_token   = process.env.AZURE_TOKEN;
+
+// global variables
+var logger = console.log;
+var opts = {
+    serviceOptions: [azure_account, azure_token], // custom arguments to azure.createBlobService
+    containerName: null, // container name in blob
+    containerOptions: { publicAccessLevel: "blob" }, // container options
+    folder: '', // path within container
+    deleteExistingBlobs: true, // true means recursively deleting anything under folder
+    concurrentUploadThreads: 50, // number of concurrent uploads, choose best for your network condition
+    zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
+    metadata: { cacheControl: 'public, max-age=31556926' }, // metadata for each uploaded file
+    testRun: false // test run - means no blobs will be actually deleted or uploaded, see log messages for details
+};
+
+var mapFilesToDeploy = function(files) {
+  return files.map(function(item) {
+    return { cwd: '.', path: item };
+  });
+};
+
+// upload nbs json files
+glob("nbs/**/*.json", null, function (er, files) {
+  opts.containerName = 'nbs';
+  deploy(opts, mapFilesToDeploy(files), logger, function(err){
+      if(err) {
+          console.log("Error deploying", err);
+      }
+      console.log('Job\'s done!');
+  });
+});
+
+// upload ncm json files
+glob("ncm/**/*.json", null, function (er, files) {
+  opts.containerName = 'ncm';
+  deploy(opts, mapFilesToDeploy(files), logger, function(err){
+      if(err) {
+          console.log("Error deploying", err);
+      }
+      console.log('Job\'s done!');
+  });
+});
