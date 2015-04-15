9b389120dd47a46f4b3d7ff42a30e7c39b493992
Gabriel Marquez <gabriel@nfe.io>
2015-04-15 17:55:09 -0300

    fix date field needs to be iso 8601 format

--- index.js
+++ index.js
@@ -2,26 +2,32 @@
 var csv = require("fast-csv"),
     fs = require("fs"),
     iconvlite = require('iconv-lite'),
-    path = require("path");
+    path = require("path")
+    util = require("util");
 
-var estados = ["AC","AL","AM","AP","BA","CE","DF","ES","GO","MA","MG","MS","MT","PA","PB","PE","PI","PR","RJ","RN","RO","RR","RS","SC","SE","SP","TO"];
+var states = ["AC","AL","AM","AP","BA","CE","DF","ES","GO","MA","MG","MS","MT","PA","PB","PE","PI","PR","RJ","RN","RO","RR","RS","SC","SE","SP","TO"];
 
-estados.forEach(function(estado) {
+var convertDateToISOString = function(date) {
+  var a = date.split('/');
+  return a[2] + "-" + a[1] + "-" + a[0];
+};
 
-  fs.mkdir("ncm/"+estado.toLowerCase(), function(e) {});
-  fs.mkdir("nbs/"+estado.toLowerCase(), function(e) {});
+states.forEach(function(state) {
 
-  var fileName = "tabelas/TabelaIBPTax"+estado+"15.1.C.csv";
+  fs.mkdir("ncm/" + state.toLowerCase(), function(e) {});
+  fs.mkdir("nbs/" + state.toLowerCase(), function(e) {});
+
+  var fileName = "tabelas/TabelaIBPTax" + state + "15.1.C.csv";
 
   var content = fs.readFile(fileName, function (err, data) {
 
     if (err)
     {
-      console.log(fileName);
+      console.log("Reading file ", fileName, err);
       throw err;
     }
 
-    console.log("Processando arquivo " + fileName);
+    console.log("Processing file " + fileName);
 
     var string = iconvlite.decode(data, 'ISO-8859-1');
 
@@ -42,21 +48,27 @@ estados.forEach(function(estado) {
                   "fonte"],
        quoteColumns: false, ignoreEmpty:true, quote:null, delimiter:';'
      })
-     .on("error", function(err){
+     .on("error", function(err) {
        console.log(fileName, err);
        throw err;
      })
-     .on("data", function(data){
-       var tipo = 'ncm';
-       if (data.tipo == 1) {
-         tipo = 'nbs';
-       }
-       fs.writeFileSync(tipo+"/"+estado.toLowerCase()+"/" + data.codigo + ".json", JSON.stringify(data));
+     .on("data", function(data) {
+       // convert date to iso 8601
+       data.vigenciainicio = convertDateToISOString(data.vigenciainicio);
+       data.vigenciafim = convertDateToISOString(data.vigenciafim);
+
+       var type = (data.tipo == 1)
+                  ? 'nbs'
+                  : 'ncm';
+       var filePath = util.format('%s/%s/%s.json', type, state.toLowerCase(), data.codigo);
+       var data = iconvlite.encode(JSON.stringify(data), 'UTF-8')
+
+       fs.writeFileSync(filePath, data);
      })
      .on("end", function(){
-         console.log("Finalizado processamento de " + fileName);
+         console.log("Processed file " + fileName);
      });
 
-  });
+  }); // end of csv
 
-});
+}); // end of state
