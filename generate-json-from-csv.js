7185764c0edf89e2015d611eaad44d67ddab234c
Gabriel Marquez <gabriel@conube.com.br>
2017-03-17 15:56:16 -0300

    Add raw data for version 17.1.B

--- generate-json-from-csv.js
+++ generate-json-from-csv.js
@@ -5,7 +5,7 @@ var csv = require("fast-csv"),
     util = require("util");
 
 // constant of version
-var version = "17.1.A";
+var version = "17.1.B";
 
 // constant of states
 var states = ["ac", "al", "am", "ap", "ba", "ce", "df", "es", "go", "ma", "mg",
