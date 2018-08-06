4ca3d1ddc8f70e35b0148c3916fdca9000f00990
Gabriel Marquez <gabriel@nfe.io>
2018-08-06 12:08:21 -0300

    Update deploy threads

--- deploy_ncm.js
+++ deploy_ncm.js
@@ -8,24 +8,28 @@ if (require("fs").existsSync(".env")) {
 
 // environment variables
 var azure_account = process.env.AZURE_ACCOUNT,
-    azure_token   = process.env.AZURE_TOKEN;
+  azure_token = process.env.AZURE_TOKEN;
 
 // global variables
 var logger = console.log;
 var opts = {
-    serviceOptions: [azure_account, azure_token], // custom arguments to azure.createBlobService
-    containerName: null, // container name in blob
-    containerOptions: { publicAccessLevel: "blob" }, // container options
-    folder: '', // path within container
-    deleteExistingBlobs: false, // true means recursively deleting anything under folder
-    concurrentUploadThreads: 50, // number of concurrent uploads, choose best for your network condition
-    zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
-    metadata: { cacheControl: 'public, max-age=31556926' }, // metadata for each uploaded file
-    testRun: false // test run - means no blobs will be actually deleted or uploaded, see log messages for details
+  serviceOptions: [azure_account, azure_token], // custom arguments to azure.createBlobService
+  containerName: null, // container name in blob
+  containerOptions: {
+    publicAccessLevel: "blob"
+  }, // container options
+  folder: '', // path within container
+  deleteExistingBlobs: false, // true means recursively deleting anything under folder
+  concurrentUploadThreads: 2, // number of concurrent uploads, choose best for your network condition
+  zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
+  metadata: {
+    cacheControl: 'public, max-age=31556926'
+  }, // metadata for each uploaded file
+  testRun: false // test run - means no blobs will be actually deleted or uploaded, see log messages for details
 };
 
-var mapFilesToDeploy = function(files) {
-  return files.map(function(item) {
+var mapFilesToDeploy = function (files) {
+  return files.map(function (item) {
     return {
       base: item.substring(0, 3),
       path: item
@@ -34,20 +38,21 @@ var mapFilesToDeploy = function(files) {
 };
 
 var states = ["ac", "al", "am", "ap", "ba", "ce", "df", "es", "go", "ma", "mg",
-              "ms", "mt", "pa", "pb", "pe", "pi", "pr", "rj", "rn", "ro", "rr",
-              "rs", "sc", "se", "sp", "to"];
+  "ms", "mt", "pa", "pb", "pe", "pi", "pr", "rj", "rn", "ro", "rr",
+  "rs", "sc", "se", "sp", "to"
+];
 
-states.forEach(function(state) {
+states.forEach(function (state) {
 
   // upload ncm json files
   glob("ncm/" + state + "/*.json", null, function (er, files) {
     opts.containerName = 'ncm';
-    deploy(opts, mapFilesToDeploy(files), logger, function(err){
-        if(err) {
-            console.log('error: ncm upload job ' + state, err);
-        } else {
-          console.log('ok: ncm upload job ' + state);
-        }
+    deploy(opts, mapFilesToDeploy(files), logger, function (err) {
+      if (err) {
+        console.log('error: ncm upload job ' + state, err);
+      } else {
+        console.log('ok: ncm upload job ' + state);
+      }
     });
 
     console.log('queued jobs to state ' + state);
